302e0eb2556d17ac83ab9682bd7c8982fca02069
Unknown <q.vanveen@student.fontys.nl>
2019-05-16 11:30:08 +0200

    Added Console and Timer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  totalTime: number = 10;
+  timeLeft = this.totalTime;
+  interval;
+
+
   constructor() { }
 
   ngOnInit() {
+    this.startTimer()
+  }
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        this.timeLeft = this.totalTime;
+      }
+    },1000)
+  }
+
+  pauseTimer() {
+    clearInterval(this.interval);
   }
 
 }
